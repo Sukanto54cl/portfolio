23c440af5bde69e1522fb46040c87b795ca74b6a
Sukanto54cl <sukantodas1993@gmail.com>
2025-12-24 02:42:45 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 export default defineConfig({
-  base: "/portfolio/",   // 👈 REQUIRED for GitHub Pages
+  base: "/portfolio/",   // 👈 ADD THIS LINE
   plugins: [react()],
   resolve: {
     alias: {
